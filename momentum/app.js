f803cb7c389dffaad01912ea372804fed08fb56a
“Winsomejoo” <tmdwn0603@naver.com>
2024-02-04 23:53:54 +0900

    feat: paintGreeting 함수 추가

--- momentum/app.js
+++ momentum/app.js
@@ -1,34 +1,29 @@
-const loginform=document.querySelector("#login-form")
-const loginInput=document.querySelector("#login-form input")
-const greeting=document.querySelector("#greeting")
+const loginform=document.querySelector("#login-form");
+const loginInput=document.querySelector("#login-form input");
+const greeting=document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME="hidden"
+const HIDDEN_CLASSNAME="hidden";
 const USERNAME_KEY="username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginform.classList.add(HIDDEN_CLASSNAME);
     const username=loginInput.value;
-    localStorage.setItem(USERNAME_KEY,username)
-    greeting.innerText= `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreeting(username);
 }  
 
 function paintGreeting(username){
     greeting.innerText= `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-
- 
 const savedUsername=localStorage.getItem(USERNAME_KEY);
 
 
-
 if(savedUsername===null){
-    loginform.classList.remove(HIDDEN_CLASSNAME)
+    loginform.classList.remove(HIDDEN_CLASSNAME);
     loginform.addEventListener("submit",onLoginSubmit);
 }else{
-    greeting.innerText= `Hello ${savedUsername}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreeting(savedUsername);
 }
