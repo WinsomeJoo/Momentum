ff7292252192bc752c916cca7c964999d679d5be
“Winsomejoo” <tmdwn0603@naver.com>
2024-02-04 23:51:10 +0900

    feat: statuful하게 log인 상태 유지하게 하기

--- momentum/app.js
+++ momentum/app.js
@@ -1,17 +1,34 @@
 const loginform=document.querySelector("#login-form")
 const loginInput=document.querySelector("#login-form input")
 const greeting=document.querySelector("#greeting")
-const HIDDEN_CLASSNAME="hidden"
 
+const HIDDEN_CLASSNAME="hidden"
+const USERNAME_KEY="username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginform.classList.add(HIDDEN_CLASSNAME);
     const username=loginInput.value;
-    greeting.innerText='Hello ${username}';
-
+    localStorage.setItem(USERNAME_KEY,username)
+    greeting.innerText= `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME)
-    
+}  
+
+function paintGreeting(username){
+    greeting.innerText= `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-loginform.addEventListener("submit",onLoginSubmit)
+
+ 
+const savedUsername=localStorage.getItem(USERNAME_KEY);
+
+
+
+if(savedUsername===null){
+    loginform.classList.remove(HIDDEN_CLASSNAME)
+    loginform.addEventListener("submit",onLoginSubmit);
+}else{
+    greeting.innerText= `Hello ${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
