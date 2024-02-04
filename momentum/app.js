422ceb1be18bec751302595b51ebd368658a0b1c
“Winsomejoo” <tmdwn0603@naver.com>
2024-02-04 18:02:57 +0900

    feat: login시 공백 및 글자 길이 유효성 검사 함수 추가

--- momentum/app.js
+++ momentum/app.js
@@ -1,3 +1,14 @@
-//2번 방식
 const loginInput=document.querySelector("#login-form input")
 const loginButton=document.querySelector("#login-form button")
+
+
+function onLogBtnClick(){
+    const userName= loginInput.value;
+    if(userName===""){
+        alert("please write your name")
+    }else if(userName.length>=15){
+        alert("your name is too long")
+    }
+}
+
+loginButton.addEventListener("click",onLogBtnClick)
